cba37ab4461d87e28c4193db1f9c986538bbdb4e
yangaowei <gaoweiyan@sohu-inc.com>
2017-06-21 19:26:50 +0800

    添加go shell

---
+++ gocmd.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"os/exec"
+)
+
+func Cmd(cmds string) (result string) {
+	log.Println("run cmd:", cmds)
+	var cmd *exec.Cmd
+	cmd = exec.Command("/bin/sh", "-c", cmds)
+	var domifstat bytes.Buffer
+	cmd.Stdout = &domifstat
+	err := cmd.Run()
+	if err != nil {
+		log.Printf("Error while exec cmd %a", err)
+		return ""
+	}
+	result = domifstat.String()
+	return
+}
+
+func main() {
+	result := Cmd("ps -ef | grep python")
+	log.Println(result)
+}

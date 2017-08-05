53f419fde198c907d9222add5c75a017ed4ac427
yangaowei <gaoweiyan@sohu-inc.com>
2017-08-05 11:38:40 +0800

    nothing

---
+++ got.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type student struct {
+	Name string
+	Age  int
+}
+
+func main() {
+	//runtime.GOMAXPROCS(1)
+	wg := sync.WaitGroup{}
+	wg.Add(20)
+	for i := 0; i < 10; i++ {
+		go func() {
+			fmt.Println("A: ", i)
+			wg.Done()
+		}()
+	}
+	for i := 0; i < 10; i++ {
+		go func(i int) {
+			fmt.Println("i: ", i)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+}

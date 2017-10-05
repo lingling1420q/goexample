d3bf6e129eb721c5e4e90467bcd1b7c197c3d283
yangaowei <gaoweiyan@sohu-inc.com>
2017-10-05 19:55:25 +0800

    go string

--- gostring.go
+++ gostring.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"log"
+	"strings"
 )
 
 func main() {
 	str := "yangaowei"
 	log.Println(str[:len(str)-1])
+
+	values := strings.SplitN("/yan", "?", 2)
+
+	log.Println(values)
 }

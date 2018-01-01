c6333d63d87b9d4910d6c1d040609ff987aa7950
yangaowei <gaoweiyan@sohu-inc.com>
2018-01-01 20:14:39 +0800

    快排的另一种实现方式

--- sort/quictsort.go
+++ sort/quictsort.go
@@ -29,6 +29,38 @@ func quickSort(a []int, low int, hight int) {
 	}
 }
 
+func quickSort02(arr []int) (result []int) {
+	size := len(arr)
+	if size == 0 {
+		return
+	} else {
+		pivot := arr[0]
+		left := func(a []int) (l []int) {
+			for _, v := range a {
+				if v < pivot {
+					l = append(l, v)
+				}
+			}
+			return
+		}(arr)
+		right := func(a []int) (r []int) {
+			for _, v := range a[1:] {
+				if v >= pivot {
+					r = append(r, v)
+				}
+			}
+			return
+		}(arr)
+
+		l := quickSort02(left)
+		r := quickSort02(right)
+		result = append(result, l...)
+		result = append(result, pivot)
+		result = append(result, r...)
+		return
+	}
+}
+
 func quick(arr []int) {
 	x := arr[0]
 	i := 0
@@ -52,8 +84,8 @@ func quick(arr []int) {
 
 func main() {
 	a := []int{3, 1, 5, 7, 2, 4, 9, 6, 10, 8}
-	quick(a)
-	// fmt.Println(a)
+	r := quickSort02(a)
+	fmt.Println(r)
 	// quickSort(a, 0, 9)
 	// fmt.Println(a)
 }

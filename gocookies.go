f301bf0b02f9cfb5063c059865476e78df10c698
闫高威 <gaoweiyan@sohu-inc.com>
2018-03-23 15:42:07 +0800

    登录豆瓣

--- gocookies.go
+++ gocookies.go
@@ -2,50 +2,66 @@ package main
 
 import (
 	//"github.com/axgle/mahonia"
+	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/http/cookiejar"
-	//"time"
-	//"strings"
+	"regexp"
+	"strings"
 )
+
+// data={
+//     'redir': 'https://www.douban.com/',
+//     'form_email':'18600189467',
+//     'form_password':'linjie20061219'
+//     #'login':u'登录'
+// }
+
+var (
+	loginUrl string = "https://accounts.douban.com/login"
+	indexUlr string = "https://www.douban.com/people/141268126/"
+)
+
+func addHeader(req *http.Request) {
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36")
+}
+
+func RxOf(pattern string, content string, index int) (rcontent string) {
+	re, _ := regexp.Compile(pattern)
+	submatch := re.FindStringSubmatch(content)
+	for i, v := range submatch {
+		if i == index {
+			rcontent = v
+			break
+		}
+	}
+	return
+}
+
+func R1(pattern string, content string) (rcontent string) {
+	return RxOf(pattern, content, 1)
+}
 
 func httpDo() {
 	client := &http.Client{}
 	cookieJar, _ := cookiejar.New(nil)
 	client.Jar = cookieJar
 
-	req, err := http.NewRequest("GET", "http://www.66ip.cn/areaindex_1/index.html", nil)
+	data := "redir=https://www.douban.com/&form_email=18600189467&form_password=linjie20061219"
+	req, err := http.NewRequest("POST", loginUrl, strings.NewReader(data))
 	if err != nil {
-		// handle error
+		fmt.Println(err)
+		return
 	}
-
-	// req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	// req.Header.Set("Cookie", "name=anny")
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
-	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
-	req.Header.Set("Host", "Host:www.66ip.cn")
-	req.Header.Set("Accept-Encoding", "gzip, deflate, sdch")
-	req.Header.Set("Referer", "http://www.66ip.cn/areaindex_1/index.html")
-	req.Header.Set("Cookie", "__cfduid=dec031f0ed6ab5afc64978bfaa48a9e0a1496720691; UM_distinctid=15c7b8161ab1-06d3d1f923b304-3062750a-fa000-15c7b8161ac32e; cf_clearance=72e6dbcd3682413fd4692076abf4da6eff2e44c8-1496731093-3600; CNZZDATA1253901093=1678483328-1496717997-http%253A%252F%252Fwww.66ip.cn%252F%7C1496728955; Hm_lvt_1761fabf3c988e7f04bec51acd4073f4=1496720696; Hm_lpvt_1761fabf3c988e7f04bec51acd4073f4=1496732616")
-
-	for _, obj := range req.Cookies() {
-		log.Println(obj.Name, ":", obj.Value, obj.Domain)
-	}
-
-	log.Println(req.Header)
-
+	addHeader(req)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	resp, err := client.Do(req)
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		// handle error
+		fmt.Println(err)
+		return
 	}
-	html := string(body)
-	//html = mahonia.NewDecoder("gbk").ConvertString(html)
-	log.Println(len(html))
-	log.Println(html)
+	h, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(h))
 }
 
 func main() {
